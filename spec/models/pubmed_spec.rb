8d69ced7847fa018d7cfefce7addd7155093b1cd
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2015-03-04 11:58:14 +0100

    Move pubmed webmock stubs closer to actual tests

--- spec/models/pubmed_spec.rb
+++ spec/models/pubmed_spec.rb
@@ -4,17 +4,6 @@ RSpec.describe Pubmed, :type => :model do
   before :each do
     WebMock.disable_net_connect!
 
-    stub_request(:get, "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=&retmode=xml").
-      with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
-      to_return(:status => 200, :body => File.new("#{Rails.root}/spec/support/adapters/pubmed-nil.xml"), :headers => {})
-
-    stub_request(:get, "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=255055741354975&retmode=xml").
-      with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
-      to_return(:status => 200, :body => File.new("#{Rails.root}/spec/support/adapters/pubmed-255055741354975.xml"), :headers => {})
-
-    stub_request(:get, "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=25505574&retmode=xml").
-      with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
-      to_return(:status => 200, :body => File.new("#{Rails.root}/spec/support/adapters/pubmed-25505574.xml"), :headers => {})
 
   end
   after :each do
@@ -22,6 +11,11 @@ RSpec.describe Pubmed, :type => :model do
   end
   describe "find_by_id" do
     context "with an existing id" do
+      before :each do
+        stub_request(:get, "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=25505574&retmode=xml").
+          with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => File.new("#{Rails.root}/spec/support/adapters/pubmed-25505574.xml"), :headers => {})
+      end
       it "should return a valid object" do
         pubmed = Pubmed.find_by_id "25505574"
         expect(pubmed.errors.messages.empty?).to be_truthy
@@ -34,12 +28,22 @@ RSpec.describe Pubmed, :type => :model do
       end
     end
     context "with a no existing id" do
+      before :each do
+        stub_request(:get, "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=255055741354975&retmode=xml").
+          with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => File.new("#{Rails.root}/spec/support/adapters/pubmed-255055741354975.xml"), :headers => {})
+      end
       it "should return a invalid object" do
         pubmed = Pubmed.find_by_id "255055741354975"
         expect(pubmed.errors.messages.empty?).to be_falsey
       end
     end
     context "with no id" do
+      before :each do
+        stub_request(:get, "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=&retmode=xml").
+          with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => File.new("#{Rails.root}/spec/support/adapters/pubmed-nil.xml"), :headers => {})
+      end
       it "should return a invalid object" do
         pubmed = Pubmed.find_by_id ""
         expect(pubmed.errors.messages.empty?).to be_falsey
