021ce4650e103a5acf7c3794fa5455ad577299a3
Per Larsson <per.larsson@ub.gu.se>
2015-03-17 11:28:26 +0100

    Added migration and model for departments2people2publication.

--- app/models/people2publication.rb
+++ app/models/people2publication.rb
@@ -1,5 +1,6 @@
 class People2publication  < ActiveRecord::Base
   belongs_to :publication
+  has_many :departments2people2publications
   
   validates :publication_id, presence: true
   validates :person_id, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150311141758) do
+ActiveRecord::Schema.define(version: 20150317101851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "departments2people2publications", force: :cascade do |t|
+    t.integer "people2publication_id"
+    t.text    "department_name"
+    t.integer "position"
+  end
 
   create_table "people2publications", force: :cascade do |t|
     t.integer "publication_id"
